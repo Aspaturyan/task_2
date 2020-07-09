498ca6c9a1a81b6898ede20b956bd6c263bb3270
Aspaturyan <67711115+Aspaturyan@users.noreply.github.com>
2020-07-10 01:04:50 +0400

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,20 @@
+let a=45;
+let b=30;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+alert(sum);
+alert(difference);
+alert(product);
+alert(quotient);
+alert(remainder);
